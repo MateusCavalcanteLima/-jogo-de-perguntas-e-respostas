f6d3d636ae0217e4930cdb52bd3c84d60b01a996
MateusCavalcanteLima <cavalcante.mateus@academico.ifpb.edu.br>
2025-01-23 20:21:13 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,22 +1,23 @@
 def jogo_perguntas():
     perguntas = {
         "Qual a linguagem que estamos usando?": "Python",
-        "Qual é a maior floresta tropical do mundo?": "Amazônia",
-        "Quem descobriu o brasil?": "Indios"
+        "Qual é a maior floresta tropical do mundo?": "Amazonia",
+        "Quem descobriu o Brasil?": "Índios"
     }
 
     for pergunta, resposta_correta in perguntas.items():
         tentativas = 0
         while tentativas < 2:
-            resposta_jogador = input(pergunta + " ").lower()
-            if resposta_jogador == resposta_correta.lower():
-                print("Resposta correta!")
+            resposta_jogador = input(pergunta + " ").strip().lower()
+            if resposta_jogador == resposta_correta.strip().lower():
+                print("Resposta correta! Vamos para a próxima pergunta.")
                 break
             else:
-                print("Resposta errada!")
                 tentativas += 1
-                if tentativas == 2:
-                    print(f"A resposta correta é: {resposta_correta}")
+                if tentativas < 2:
+                    print("Resposta errada! Você tem mais 1 tentativa.")
+                else:
+                    print(f"Resposta errada! A resposta correta é: {resposta_correta}")
 
 if __name__ == "__main__":
     jogo_perguntas()
